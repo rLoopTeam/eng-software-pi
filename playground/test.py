ea58eb1f07239fac0e0e8a5ccc58a27c65ad9adb
root <root@navio-rpi.(none)>
2015-11-17 01:52:41 +0000

    Awoerp - Putting all of the original files into their respective repository locations.

---
+++ playground/test.py
@@ -0,0 +1,13 @@
+
+def main():
+	ans = raw_input('Are you a butt? [Y/N]: ')
+	
+	if(ans == 'Y' or ans == 'y' or ans == 'yes'):
+		print 'Hello butt face'
+	elif(ans == 'N' or ans == 'n' or ans == 'no'):
+		print 'Hello "not" butt face'
+	else:
+		print ('ssssSSSSssssSS')
+
+
+main()
