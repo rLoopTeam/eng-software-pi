6f9641c8cf4155d993f5f9829475fc66dfe6ab0a
Piense <piense@gmail.com>
2016-03-23 23:20:16 -0700

    Moved I2C module to it's own repo

--- bsc-slave-master/.tmp_versions/bsc-slave.mod
+++
@@ -1,2 +0,0 @@
-/home/pi/Downloads/bsc-slave-master/bsc-slave.ko
-/home/pi/Downloads/bsc-slave-master/bsc-slave.o
--- bsc-slave-master/bsc-slave.h
+++
@@ -1,3 +0,0 @@
-#define I2C_SLAVE       0x0703  /* Use this slave address */
-#define I2C_SLAVE_FORCE 0x0706  /* Use this slave address, even if it*/
-
