43505a8895a33a366bef062267d762ca5ae80d64
Paul <paullehenaff@msn.com>
2016-04-20 00:25:18 +0100

    Got UDP telemetry working

--- comm_module/groundstation.py
+++ comm_module/groundstation.py
@@ -18,25 +18,21 @@ def main():
 	"""
 	groundstation_receiver = socket.socket(	socket.AF_INET, # Internet
 	                     					socket.SOCK_DGRAM) # UDP
-	groundstation_receiver.bind((nodeList.get_node_as_tuple('gs_in')[1], nodeList.get_node_as_tuple('gs_in')[2]))
+	groundstation_receiver.bind((nl.get_address_as_tuple('gs_in')[1], nl.get_address_as_tuple('gs_in')[2]))
 
 	"""
 	TCP commander sender/receiver socket
 	"""
-	groundstation_sender = ctx.socket(zmq.PUSH)
-	groundstation_sender.bind(nodeList.get_node('gs_out'))
+	# groundstation_sender = ctx.socket(zmq.PUSH)
+	# groundstation_sender.bind(nl.get_address('gs_out'))
 
 	while True:
-		command = "MOVE_FORWARD"
-		groundstation_sender.send(command)
-		print_out("GS: COMMAND: %s"%command)
+		# command = "MOVE_FORWARD"
+		# groundstation_sender.send(command)
+		# print_out("GS: COMMAND: %s"%command)
 	
-		try:
-
-			message, addr = groundstation_receiver.recvfrom(1024) # buffer size is 1024 bytes
-			print_out("GS: TELEMETRY: %s"%message)
-		except zmq.ZMQError, e:
-			pass
+		message, addr = groundstation_receiver.recvfrom(1024) # buffer size is 1024 bytes
+		print_out("GS: TELEMETRY: %s"%message)
 
 		time.sleep(2)
 
